2c489bdd417fd023b05a7a557368eb49200e92a2
Michal Margiel <michal.margiel@gmail.com>
2013-09-24 20:39:29 +0200

    changes in unidirectional binding

--- js/directives.js
+++ js/directives.js
@@ -1,7 +1,7 @@
 angular.module("directives", []).directive("mmTest", function () {
     return {
         scope: {text: '@mmAtr', dBi : '=mmBi'},
-        template: "<div>{{ text }}<br><input type='text' ng-model='dBi'> </div>"
+        template: "<div><input type='text' ng-model='text'><br><input type='text' ng-model='dBi'> </div>"
     }
 });
 angular.module("training", ["directives"]);
