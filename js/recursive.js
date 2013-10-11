e3b28bf49cd2d8f5aeaea67965c3d37416a39981
Michal Margiel <michal.margiel@gmail.com>
2013-10-11 23:31:25 +0200

    recursive directive NOT working

--- js/recursive.js
+++ js/recursive.js
@@ -1,24 +1,12 @@
 angular.module("training", [])
     .directive("mmTree", function () {
         return {
+            scope: {items: '='},
             restrict: 'E',
-            template: "<div>" +
-                "<script type='text/ng-template' id='tree.html'>" +
+            template: "<div ng-repeat='item in items'>" +
                 "{{item.name}}" +
-                "<div ng-include=\"'tree.html'\" ng-repeat='item in item.children'></div>" +
-                "</script>" +
-                "" +
-                "<div ng-repeat='item in items' ng-include=\"'tree.html'\"></div>" +
-                "</div>",
-            link: function (scope) {
-                scope.items = [
-                    {name: "a", children: [
-                        {name: "c"},
-                        {name: "d"}
-                    ]},
-                    {name: "b", children: []}
-                ];
-            }
+                "<mm-tree items='item.children'></mm-tree>" +
+                "</div>"
 
         }
     })
